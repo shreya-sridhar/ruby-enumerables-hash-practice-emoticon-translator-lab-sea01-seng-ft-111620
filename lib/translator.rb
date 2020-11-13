f88109ba6bd3b93e531a5c9320ddc90a2793a2ae
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-13 20:27:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,13 +4,19 @@ require "yaml"
 def load_library(path)
   # code goes here
   emoticons = YAML.load_file(path)
-  emoticons.each_with_object({}) do |(key,value), final_hash|
-    final_hash[key] = {:english => value[0], :japanese => value[1]}
+  emoticon_hash = Hash.new
+
+  emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_meaning"] = Hash.new
+
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
   end
   emoticon_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emotion)
   # code goes here
     emoticon_hash = load_library(path)
   result = emoticon_hash["get_emoticon"][emoticon]
@@ -20,7 +26,7 @@ def get_japanese_emoticon
   result
 end
 
-def get_english_meaning
+def get_english_meaning(path, emotion)
   # code goes here
   emoticon_hash = load_library(path)
   result = emoticon_hash["get_meaning"][emoticon]
