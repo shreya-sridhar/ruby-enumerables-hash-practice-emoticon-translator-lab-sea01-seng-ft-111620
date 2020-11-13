44a2a31aecd6ce56c592cb32a6c61a991b2ab9b6
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-13 20:18:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,5 +28,10 @@ end
 
 def get_english_meaning
   # code goes here
-  
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
 end
