95c4e4c9bb9f926d65f2d55d7a24fc24d1413d43
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-13 20:10:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library
   emoticon_array = YAML.load_file("../lib/emoticons.yml")
   emoticon_array.each_with_object({}) do |(key,value), final_hash|
     final_hash[key] = {:english => value[0], :japanese => value[1]}
+  end
   emoticon_array
 end
 
